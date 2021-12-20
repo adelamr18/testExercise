d9b7fc4151b3dcfd0fec54d1269020d82d221e9f
a.gebril <a.gebril@metro-markets.de>
2021-12-20 04:39:08 +0100

    Add drag and drop functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -79,5 +79,64 @@ export class AppComponent implements AfterViewInit {
     objectDataCopy.map((obj:ObjectDetails) => obj.isRowHighlighted = false);
   }
   
-  
+  //after trying out drag and drop functionality the other logic wont work because it needs a bit of reworking and because the array 
+  //indexes need adjustments so it needs some minor adjustments to work perfectly the only thing that was missing was to set the 
+  //modified objects after drag and drop to objectData
+  private configureDragAndDropConfiguration = (): void => {
+    const draggables = document.querySelectorAll('.draggable');
+    const containers = document.querySelectorAll('.objects-details-list');
+
+    draggables.forEach(draggable => {
+      draggable.addEventListener('dragstart', () => {
+        draggable.classList.add('dragging');
+      })
+    });
+
+    draggables.forEach(draggable => {
+      draggable.addEventListener('dragend', () => {
+        const draggables = document.getElementsByTagName('li') 
+        let obj = {
+          labelName: '',
+          labelId: 0,
+          testClass: 'fa-arrows-alt',
+          isRowHighlighted: false
+        };
+        draggable.classList.remove('dragging');
+      Array.from(draggables).forEach(function(element, index) {
+        obj.labelName = element.innerText;
+        obj.labelId = parseInt(element.innerText);
+    });      
+      })
+    });
+
+    containers.forEach(container => {
+      container.addEventListener('dragover', (e: any) => {
+      e.preventDefault();
+      const afterElement = this.getDragAfterElement(container, e.clientY);
+      const draggable: any = document.querySelector('.dragging');
+      if(afterElement == null){
+       container.appendChild(draggable);
+      }
+      });
+    })
+  }
+
+  getDragAfterElement = (container: any, y: any): void => {
+    const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')];
+    return draggableElements.reduce((closest: any, child: any) => {
+      const box = child.getBoundingClientRect();
+      const offset = y - box.top - box.height / 2;
+      if (offset < 0 && offset > closest.offset) {
+        return { offset, element: child };
+      } else {
+        return closest;
+      }
+    },
+      {
+        offset: Number.NEGATIVE_INFINITY
+      }).element;
+  }
+
+
+
 }
