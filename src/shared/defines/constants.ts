7a79d4f469089dc85da31fe2ffd73b2cd177e011
a.gebril <a.gebril@metro-markets.de>
2021-12-20 02:45:27 +0100

    Create relevant mock data and its interface

---
+++ src/shared/defines/constants.ts
@@ -0,0 +1,22 @@
+export const testData = [
+  {
+    labelName: 'Item 1',
+    labelId: 0,
+    isRowHighlighted: false
+  },
+  {
+    labelName: 'Item 2',
+    labelId: 1,
+    isRowHighlighted: false
+  },
+  {
+    labelName: 'Item 3',
+    labelId: 2,
+    isRowHighlighted: false
+  },
+  {
+    labelName: 'Item 4',
+    labelId: 3,
+    isRowHighlighted: false
+  },
+];
---
+++ src/shared/models/object-details.model.ts
@@ -0,0 +1,6 @@
+
+export interface ObjectDetails {
+    labelName: string;
+    labelId: number;
+    isRowHighlighted: boolean;
+  }
