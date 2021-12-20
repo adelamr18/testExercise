65fde83bbefcbb3932f1799689b51489339b852e
a.gebril <a.gebril@metro-markets.de>
2021-12-20 04:38:17 +0100

    Add sorting view and functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,14 +28,56 @@ export class AppComponent implements AfterViewInit {
   }
 
   public hightlightRow = (selectedObject: ObjectDetails): void => {
+    this.resetRowsSelection();
     this.selectedRow = selectedObject;
     selectedObject.isRowHighlighted = !selectedObject.isRowHighlighted;
   }
 
- 
+  public arrangeDescendingly = (): void => {
+    if (this.objectData.length) {
+      if (this.selectedRow.labelId > this.objectData[0].labelId) {
+        this.sortObjects(0);
+      }
+    }
+  }
 
- 
+  public arrangeAscendingly = (): void => {
+    if (this.objectData.length) {
+      if (this.selectedRow.labelId < this.objectData[this.objectData.length -1].labelId) {
+        this.sortObjects(this.objectData.length -1);
+      }
+    }
+  }
 
+  private sortObjects = (startIndex: number): void => {
+    const selectedIndex = this.objectData.indexOf(this.selectedRow)
+    const tempCell = this.objectData[startIndex];
+    this.objectData[startIndex] = this.selectedRow;
+    this.objectData[selectedIndex] = tempCell; 
+  }
 
+  public arrangeOneLevelDesc = (): void => {
+    if (this.objectData.length) {
+      const selectedIndex = this.objectData.indexOf(this.selectedRow)
+      if (this.selectedRow.labelId > this.objectData[selectedIndex -1].labelId) {
+        this.sortObjects(selectedIndex - 1);
+      }
+    }
+  }
 
+  public arrangeOneLevelAsc = (): void => {
+    if (this.objectData.length) {
+      const selectedIndex = this.objectData.indexOf(this.selectedRow)
+      if (this.selectedRow.labelId < this.objectData[selectedIndex +1].labelId) {
+        this.sortObjects(selectedIndex + 1);
+      }
+    }
+  }
+
+  private resetRowsSelection = (): void => {
+    const objectDataCopy = [...this.objectData];
+    objectDataCopy.map((obj:ObjectDetails) => obj.isRowHighlighted = false);
+  }
+  
+  
 }
