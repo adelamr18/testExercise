e641e159a206fc62b32103bf37dc57540669629d
a.gebril <a.gebril@metro-markets.de>
2021-12-21 02:05:09 +0100

    Implement cleanup and finish relevant drag and drop logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, AfterViewInit } from '@angular/core';
-import { testData } from 'src/shared/defines/constants';
+import { objectsData, ICON_CLASS } from 'src/shared/defines/constants';
 import { ObjectDetails } from 'src/shared/models/object-details.model';
 
 @Component({
@@ -8,12 +8,13 @@ import { ObjectDetails } from 'src/shared/models/object-details.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements AfterViewInit {
-  public objectData: ObjectDetails[] = testData;
+  public objectData: ObjectDetails[] = objectsData;
   private selectedRow: any = {};
   public isRowHighlighted = false;
   public modifiedObjectsAFterDrag: ObjectDetails[] = [];
 
   ngAfterViewInit() {
+    this.configureDragAndDropConfiguration();
   }
 
   public trackById = (index: number, item: ObjectDetails): number => {
@@ -25,13 +26,13 @@ export class AppComponent implements AfterViewInit {
     let objectDataCopy = [...this.objectData];
     objectDataCopy.splice(index, 1);
     this.objectData = objectDataCopy;
-  }
+  };
 
   public hightlightRow = (selectedObject: ObjectDetails): void => {
     this.resetRowsSelection();
     this.selectedRow = selectedObject;
     selectedObject.isRowHighlighted = !selectedObject.isRowHighlighted;
-  }
+  };
 
   public arrangeDescendingly = (): void => {
     if (this.objectData.length) {
@@ -39,104 +40,129 @@ export class AppComponent implements AfterViewInit {
         this.sortObjects(0);
       }
     }
-  }
+  };
 
   public arrangeAscendingly = (): void => {
     if (this.objectData.length) {
-      if (this.selectedRow.labelId < this.objectData[this.objectData.length -1].labelId) {
-        this.sortObjects(this.objectData.length -1);
+      if (
+        this.selectedRow.labelId <
+        this.objectData[this.objectData.length - 1].labelId
+      ) {
+        this.sortObjects(this.objectData.length - 1);
       }
     }
-  }
+  };
 
   private sortObjects = (startIndex: number): void => {
-    const selectedIndex = this.objectData.indexOf(this.selectedRow)
+    const selectedIndex = this.objectData.indexOf(this.selectedRow);
     const tempCell = this.objectData[startIndex];
     this.objectData[startIndex] = this.selectedRow;
-    this.objectData[selectedIndex] = tempCell; 
-  }
+    this.objectData[selectedIndex] = tempCell;
+  };
 
   public arrangeOneLevelDesc = (): void => {
     if (this.objectData.length) {
-      const selectedIndex = this.objectData.indexOf(this.selectedRow)
-      if (this.selectedRow.labelId > this.objectData[selectedIndex -1].labelId) {
+      const selectedIndex = this.objectData.indexOf(this.selectedRow);
+      if (
+        this.selectedRow.labelId > this.objectData[selectedIndex - 1]?.labelId
+      ) {
         this.sortObjects(selectedIndex - 1);
       }
     }
-  }
+  };
 
   public arrangeOneLevelAsc = (): void => {
     if (this.objectData.length) {
-      const selectedIndex = this.objectData.indexOf(this.selectedRow)
-      if (this.selectedRow.labelId < this.objectData[selectedIndex +1].labelId) {
+      const selectedIndex = this.objectData.indexOf(this.selectedRow);
+      if (
+        this.selectedRow.labelId < this.objectData[selectedIndex + 1]?.labelId
+      ) {
         this.sortObjects(selectedIndex + 1);
       }
     }
-  }
+  };
 
   private resetRowsSelection = (): void => {
     const objectDataCopy = [...this.objectData];
-    objectDataCopy.map((obj:ObjectDetails) => obj.isRowHighlighted = false);
-  }
-  
-  //after trying out drag and drop functionality the other logic wont work because it needs a bit of reworking and because the array 
-  //indexes need adjustments so it needs some minor adjustments to work perfectly the only thing that was missing was to set the 
-  //modified objects after drag and drop to objectData
+    objectDataCopy.map((obj: ObjectDetails) => (obj.isRowHighlighted = false));
+  };
+
   private configureDragAndDropConfiguration = (): void => {
     const draggables = document.querySelectorAll('.draggable');
     const containers = document.querySelectorAll('.objects-details-list');
 
-    draggables.forEach(draggable => {
+    draggables.forEach((draggable) => {
       draggable.addEventListener('dragstart', () => {
         draggable.classList.add('dragging');
-      })
-    });
-
-    draggables.forEach(draggable => {
-      draggable.addEventListener('dragend', () => {
-        const draggables = document.getElementsByTagName('li') 
-        let obj = {
-          labelName: '',
-          labelId: 0,
-          testClass: 'fa-arrows-alt',
-          isRowHighlighted: false
-        };
-        draggable.classList.remove('dragging');
-      Array.from(draggables).forEach(function(element, index) {
-        obj.labelName = element.innerText;
-        obj.labelId = parseInt(element.innerText);
-    });      
-      })
-    });
-
-    containers.forEach(container => {
-      container.addEventListener('dragover', (e: any) => {
-      e.preventDefault();
-      const afterElement = this.getDragAfterElement(container, e.clientY);
-      const draggable: any = document.querySelector('.dragging');
-      if(afterElement == null){
-       container.appendChild(draggable);
-      }
       });
-    })
-  }
+    });
 
-  getDragAfterElement = (container: any, y: any): void => {
-    const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')];
-    return draggableElements.reduce((closest: any, child: any) => {
-      const box = child.getBoundingClientRect();
-      const offset = y - box.top - box.height / 2;
-      if (offset < 0 && offset > closest.offset) {
-        return { offset, element: child };
-      } else {
-        return closest;
-      }
-    },
+    draggables.forEach((draggable) => {
+      draggable.addEventListener('dragend', () => {
+        this.modifiedObjectsAFterDrag = [];
+        const listElements = this.getListElementsAfterDrag();
+        this.formulateObjectsData(listElements);
+        this.objectData = JSON.parse(JSON.stringify([...this.modifiedObjectsAFterDrag]));
+      });
+    });
+
+    containers.forEach((container) => {
+      container.addEventListener('dragover', (e: any) => {
+        e.preventDefault();
+        const afterElement = this.getDragAfterElement(container, e.clientY);
+        const draggable: any = document.querySelector('.dragging');
+        if (afterElement == null) {
+          container.appendChild(draggable);
+        }
+      });
+    });
+  };
+
+  private getDragAfterElement = (container: any, y: any): void => {
+    const draggableElements = [
+      ...container.querySelectorAll('.draggable:not(.dragging)'),
+    ];
+    return draggableElements.reduce(
+      (closest: any, child: any) => {
+        const box = child.getBoundingClientRect();
+        const offset = y - box.top - box.height / 2;
+        if (offset < 0 && offset > closest.offset) {
+          return { offset, element: child };
+        } else {
+          return closest;
+        }
+      },
       {
-        offset: Number.NEGATIVE_INFINITY
-      }).element;
+        offset: Number.NEGATIVE_INFINITY,
+      }
+    ).element;
+  };
+
+  private getListElementsAfterDrag = (): string[] => {
+    const draggables = document.getElementsByTagName('li');
+    let values: string[] = [];
+    Array.from(draggables).forEach(function (element) {
+      if (element.innerText) {
+        values.push(element.innerText);
+      }
+    });
+    return values;
   }
 
+  private formulateObjectsData = (listElements: string[]): void => {
+    for (var i = 0; i < listElements.length; i++) {
+      let obj = {
+        labelName: '',
+        labelId: 0,
+        isRowHighlighted: false,
+        iconClass: ICON_CLASS
+      }
+      obj.labelName = listElements[i];
+      obj.labelId = parseInt(listElements[i + 1]);
+      this.modifiedObjectsAFterDrag.push(obj);
+      i = i + 1;
+    }
+  }
 
 
 }
